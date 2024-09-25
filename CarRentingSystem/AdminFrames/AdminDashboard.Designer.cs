4dd04222eece9f78b83874547f56627343f833ee
Nazrul Islam <79792749+nazrulislam01865@users.noreply.github.com>
2024-09-26 00:11:37 +0600

    Add files via upload
    admin

---
+++ CarRentingSystem/AdminFrames/AdminDashboard.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace CarRentingSystem.AdminFrames
+{
+    partial class AdminDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cusButton4 = new CustomControls.RJControls.CusButton();
+            this.cusButton3 = new CustomControls.RJControls.CusButton();
+            this.cusButton2 = new CustomControls.RJControls.CusButton();
+            this.cusButton1 = new CustomControls.RJControls.CusButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(381, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "User update";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(553, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Show Data";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(381, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Update Rent";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(564, 339);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Delete";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(307, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 17);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hi, ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(330, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Name";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(645, 374);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Logout";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = global::CarRentingSystem.Properties.Resources.logo2;
+            this.pictureBox2.Location = new System.Drawing.Point(676, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // cusButton4
+            // 
+            this.cusButton4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton4.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton4.BackgroundImage = global::CarRentingSystem.Properties.Resources.wheel;
+            this.cusButton4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton4.BorderRadius = 20;
+            this.cusButton4.BorderSize = 0;
+            this.cusButton4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cusButton4.FlatAppearance.BorderSize = 0;
+            this.cusButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cusButton4.ForeColor = System.Drawing.Color.White;
+            this.cusButton4.Location = new System.Drawing.Point(514, 236);
+            this.cusButton4.Name = "cusButton4";
+            this.cusButton4.Size = new System.Drawing.Size(142, 96);
+            this.cusButton4.TabIndex = 4;
+            this.cusButton4.TextColor = System.Drawing.Color.White;
+            this.cusButton4.UseVisualStyleBackColor = false;
+            this.cusButton4.Click += new System.EventHandler(this.cusButton4_Click);
+            // 
+            // cusButton3
+            // 
+            this.cusButton3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton3.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton3.BackgroundImage = global::CarRentingSystem.Properties.Resources.wheel;
+            this.cusButton3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton3.BorderRadius = 20;
+            this.cusButton3.BorderSize = 0;
+            this.cusButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cusButton3.FlatAppearance.BorderSize = 0;
+            this.cusButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cusButton3.ForeColor = System.Drawing.Color.White;
+            this.cusButton3.Location = new System.Drawing.Point(345, 236);
+            this.cusButton3.Name = "cusButton3";
+            this.cusButton3.Size = new System.Drawing.Size(142, 96);
+            this.cusButton3.TabIndex = 3;
+            this.cusButton3.TextColor = System.Drawing.Color.White;
+            this.cusButton3.UseVisualStyleBackColor = false;
+            this.cusButton3.Click += new System.EventHandler(this.cusButton3_Click);
+            // 
+            // cusButton2
+            // 
+            this.cusButton2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton2.BackgroundImage = global::CarRentingSystem.Properties.Resources.wheel;
+            this.cusButton2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton2.BorderRadius = 20;
+            this.cusButton2.BorderSize = 0;
+            this.cusButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cusButton2.FlatAppearance.BorderSize = 0;
+            this.cusButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cusButton2.ForeColor = System.Drawing.Color.White;
+            this.cusButton2.Location = new System.Drawing.Point(514, 91);
+            this.cusButton2.Name = "cusButton2";
+            this.cusButton2.Size = new System.Drawing.Size(142, 96);
+            this.cusButton2.TabIndex = 2;
+            this.cusButton2.TextColor = System.Drawing.Color.White;
+            this.cusButton2.UseVisualStyleBackColor = false;
+            this.cusButton2.Click += new System.EventHandler(this.cusButton2_Click);
+            // 
+            // cusButton1
+            // 
+            this.cusButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton1.BackgroundImage = global::CarRentingSystem.Properties.Resources.wheel;
+            this.cusButton1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(130)))), ((int)(((byte)(192)))));
+            this.cusButton1.BorderRadius = 20;
+            this.cusButton1.BorderSize = 0;
+            this.cusButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cusButton1.FlatAppearance.BorderSize = 0;
+            this.cusButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cusButton1.ForeColor = System.Drawing.Color.White;
+            this.cusButton1.Location = new System.Drawing.Point(345, 91);
+            this.cusButton1.Name = "cusButton1";
+            this.cusButton1.Size = new System.Drawing.Size(142, 96);
+            this.cusButton1.TabIndex = 1;
+            this.cusButton1.TextColor = System.Drawing.Color.White;
+            this.cusButton1.UseVisualStyleBackColor = false;
+            this.cusButton1.Click += new System.EventHandler(this.cusButton1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CarRentingSystem.Properties.Resources.adminDash_1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AdminDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(201)))), ((int)(((byte)(215)))));
+            this.ClientSize = new System.Drawing.Size(700, 400);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cusButton4);
+            this.Controls.Add(this.cusButton3);
+            this.Controls.Add(this.cusButton2);
+            this.Controls.Add(this.cusButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AdminDashboard";
+            this.Text = "AdminDashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private CustomControls.RJControls.CusButton cusButton1;
+        private CustomControls.RJControls.CusButton cusButton2;
+        private CustomControls.RJControls.CusButton cusButton3;
+        private CustomControls.RJControls.CusButton cusButton4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ CarRentingSystem/AdminFrames/UserData.cs
@@ -0,0 +1,164 @@
+﻿using CarRentalSystemRepository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentingSystem.AdminFrames
+{
+    public partial class UserData : Form
+    {
+        private string name;
+        private DataGridView dataGridViewCustomers;
+        private DataGridView dataGridViewDrivers;
+        public UserData(string name)
+        {
+            InitializeComponent();
+            this.name = name;
+            LoadCustomersAndDrivers();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            AdminDashboard adminDashboard = new AdminDashboard(name);
+            adminDashboard.Show();
+            this.Hide();
+        }
+        /*private void LoadCustomers()
+        {
+            using (SqlConnection connection = new SqlConnection("your_connection_string"))
+            {
+                string query = "SELECT CustomerID, Name, Email, PhoneNumber FROM Customers";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable customerTable = new DataTable();
+                adapter.Fill(customerTable);
+
+                dataGridViewCustomers.DataSource = customerTable;
+            }
+        }
+
+        private void LoadDrivers()
+        {
+            using (SqlConnection connection = new SqlConnection("your_connection_string"))
+            {
+                string query = "SELECT DriverID, Name, LicenseNumber, PhoneNumber FROM Drivers";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable driverTable = new DataTable();
+                adapter.Fill(driverTable);
+
+                dataGridViewDrivers.DataSource = driverTable;
+            }
+        }
+
+        private void AdminDashboard_Load(object sender, EventArgs e)
+        {
+            LoadCustomers(); // Load customer data on form load
+            LoadDrivers();   // Load driver data on form load
+        }*/
+
+
+        private void LoadCustomersAndDrivers()
+
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Documents\car.mdf;Integrated Security=True;Connect Timeout=30";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Sql Query to show Customers list
+                string customerQuery = @"
+                SELECT 
+                FullName, 
+                Email, 
+                Phone, 
+                Address, 
+                Username, 
+                Password 
+                FROM Customer";
+
+                // Sql Query to show Drivers List
+                string driverQuery = @"
+                SELECT 
+                FullName, 
+                Email, 
+                Phone, 
+                LicenceNo, 
+                CarType, 
+                CarNo, 
+                NID, 
+                Username, 
+                Password 
+                FROM Driver";
+
+                // Load Customers
+                SqlDataAdapter customerAdapter = new SqlDataAdapter(customerQuery, connection);
+                DataTable customerTable = new DataTable();
+                customerAdapter.Fill(customerTable);
+
+                // Add additional driver-specific columns to customer table
+                customerTable.Columns.Add("LicenceNo", typeof(string));
+                customerTable.Columns.Add("CarType", typeof(string));
+                customerTable.Columns.Add("CarNo", typeof(string));
+                customerTable.Columns.Add("NID", typeof(string));
+
+                // Set default values for driver-specific columns in customer table
+                foreach (DataRow row in customerTable.Rows)
+                {
+                    row["LicenceNo"] = DBNull.Value;
+                    row["CarType"] = DBNull.Value;
+                    row["CarNo"] = DBNull.Value;
+                    row["NID"] = DBNull.Value;
+                }
+
+                // Load Drivers
+                SqlDataAdapter driverAdapter = new SqlDataAdapter(driverQuery, connection);
+                DataTable driverTable = new DataTable();
+                driverAdapter.Fill(driverTable);
+
+                // Add additional customer-specific columns to driver table
+                driverTable.Columns.Add("Address", typeof(string));
+
+                // Set default values for customer-specific columns in driver table
+                foreach (DataRow row in driverTable.Rows)
+                {
+                    row["Address"] = DBNull.Value;
+                }
+
+                // Combine both tables into one DataTable
+                DataTable combinedTable = customerTable.Clone(); // Create schema from customerTable
+
+                foreach (DataRow row in customerTable.Rows)
+                {
+                    combinedTable.ImportRow(row); // Add customer rows
+                }
+
+                foreach (DataRow row in driverTable.Rows)
+                {
+                    combinedTable.ImportRow(row); // Add driver rows
+                }
+
+                // Set combined data to the DataGridView
+                dataGridView1.DataSource = combinedTable;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+      /*  private void UserData_Load(object sender, EventArgs e)
+        {
+            LoadCustomersAndDrivers(); // Load combined customer and driver data on form load
+        }*/
+    }
+}
